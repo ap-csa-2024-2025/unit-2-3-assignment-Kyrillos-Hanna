feefab2235559630fefe7e34ab8452526fd3e0d6
Kyrillos-Hanna <kyrohanna90@gmail.com>
2024-09-27 10:51:30 -0700

    worked on 2.3

--- Classwork.java
+++ Classwork.java
@@ -5,6 +5,27 @@ public class Classwork
     // test your code here
   }
 
-  // write solutions to problems below
+  public static String firstLastN(String word, int n) {
+    String output = new String();
+
+    String firstN = word.substring(0, n);
+
+    int indexOfNToLast = word.length() - n;
+    String lastN = word.substring(indexOfNToLast);
+
+    output = firstN + lastN;
+    return output;
+  }
+
+  public static String delEnd(String word, int n) {
+    return word = word.substring(0, n-1);
+  }
+
+  public static int compareLower(String word1, String word2) {
+    word1 = word1.toLowerCase();
+    word2 = word2.toLowerCase();
+  }
+
+
 
 }
